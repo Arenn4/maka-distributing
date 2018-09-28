d46166815de0b189c98ab224a0e938e3595a0b5f
Julee <jbleyerwhite@gmail.com>
2018-09-28 16:38:51 -0600

    updates

--- client/src/components/Brands.js
+++ client/src/components/Brands.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Brands = () => {
     return (
         <div>
-            Brands
+            <h1>Our Brands</h1>
+            <ul>
+                <li> Bent River Brewing</li>
+                    <p> name </p>
+                    <p> links</p>
+                    <p> logo </p>
+                <li>Blue Blood Brewing</li>
+                <li>Bur Oak Brewing</li>
+                <li>Logsdon Farmhouse Ales</li>
+                <li>People's Brewing</li>
+                <li>Trinit Brewing</li>
+                <li>Wyldewood Cellars</li>
+                <li>Shcorschbrau</li>
+                <li>Tony Chachere's</li>
+            </ul>
         </div>
     );
 };
