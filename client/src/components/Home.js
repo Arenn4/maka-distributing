f0956e73c5fb822fd45e51580951b99582143ff6
Julee <jbleyerwhite@gmail.com>
2018-09-27 23:32:13 -0600

    home0927

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,9 +3,21 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            home
+            <h1>AJ Maka Distributing</h1>
+            {/* <img src="https://images.unsplash.com/photo-1515104770-89f4214ab47b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2748ba25866bf78591f534a7a6426ab0&auto=format&fit=crop&w=800&q=60"/> */}
+
+            {/* <img src="https://images.unsplash.com/photo-1498830204877-434556b153df?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=87bf5545cab0e4c06c71e62bcd80a3f3&auto=format&fit=crop&w=2250&q=80"/> */}
+            <img src="https://images.unsplash.com/photo-1495878087991-9bd9b7545f5f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1fb31252c928901552abadb234e8b82b&auto=format&fit=crop&w=1950&q=80"/>
+            
+                <h2>Craft Beer Starts with Us</h2>
+                <img src="Logo"/>
+                    <h3>Growing YOUR craft beer brand in Chicagoland</h3>
+                    <img src="backgroud for Company Logo"/>
         </div>
     );
 };
 
 export default Home;
+
+
+
