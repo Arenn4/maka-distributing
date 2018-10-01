be9619fcf8aa426b7a2687ccd4358105ce7c037e
Julee <jbleyerwhite@gmail.com>
2018-10-01 17:25:06 -0600

    serverupdate

--- models/user.js
+++ models/user.js
@@ -1,20 +1,20 @@
-const mongoose = require('nmongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    }
-})
+  username: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
