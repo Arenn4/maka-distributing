fbfca2529544ba9ccfd28076538b8e5ec52dcbef
Alex <a.renn@yahoo.com>
2018-10-02 11:46:57 -0600

    adding routing to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+import Main from './components/Main';
 import Login from './components/Login'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
+      <div>
         <Switch>
-          <Route exact path='/' component={ Home } />
-          <Route path='/login' component={ Login } />
+          <Route exact path='/' component={ Main } />
+          <Route path='/login' component={ Login } /> 
+          <Route path='/admin' component={ Admin } />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
