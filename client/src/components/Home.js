1c2db3af3090f83bce57953eb82231f2e77e642a
Alex <a.renn@yahoo.com>
2018-10-05 09:10:58 -0600

    last minute changes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="a">
       <div className="home">
-        <img className="homeImg" src={logo} width={200}></img>
+        <img className="homeImg" src={logo} width={200} alt=""></img>
         <h1 className="homeh1">AJ Maka Distributing</h1>
         <h4 className="homeh4">GROWING YOUR BRAND IN CHICAGOLAND</h4>
       </div>
